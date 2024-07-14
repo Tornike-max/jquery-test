0c4d53fc465c7dfe783b48294004e0aa9e5ec77f
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-07-14 17:39:30 +0400

    i added show/hide password functionality using jquery

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-$(document).ready(() => {
-  const page_url = window.location.href;
-  const page_id = page_url.slice(page_url.lastIndexOf("#") + 1);
+// $(document).ready(() => {
+//   const page_url = window.location.href;
+//   const page_id = page_url.slice(page_url.lastIndexOf("#") + 1);
 
-  if (page_id === "section3") {
-    $("html,body").animate({
-      scrollTop: $(`#scroll-${page_id}`).offset().top - 20,
-    });
-  }
+//   if (page_id === "section3") {
+//     $("html,body").animate({
+//       scrollTop: $(`#scroll-${page_id}`).offset().top - 20,
+//     });
+//   }
 
-  if (page_id === "post") {
-    $("html,body").animate({
-      scrollTop: $(`#scroll-${page_id}`).offset().top,
-    });
+//   if (page_id === "post") {
+//     $("html,body").animate({
+//       scrollTop: $(`#scroll-${page_id}`).offset().top,
+//     });
+//   }
+// });
+
+let viewLogin = false;
+const changepwdtype = () => {
+  let getPwdType = $("#viewPwd");
+
+  if (viewLogin === false) {
+    getPwdType.attr("type", "text");
+    viewLogin = true;
+  } else if (viewLogin === true) {
+    getPwdType.attr("type", "password");
+    viewLogin = false;
   }
-});
+};
